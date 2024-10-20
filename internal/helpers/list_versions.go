6c6ab08db61cb7cece07d196e7371163ea67b726
Tchoupinax <corentinfiloche@hotmail.fr>
2024-10-20 21:19:13 +0200

    feat: add support to list version on DockerHub

--- internal/helpers/list_versions.go
+++ internal/helpers/list_versions.go
@@ -11,22 +11,51 @@ import (
 	"github.com/go-logr/logr"
 )
 
+type DockerHubAuth struct {
+	username string
+	password string
+}
+
 func ListVersion(
 	logger logr.Logger,
 	sourceName string,
 	matchingString string,
 	allowCandidateRelease bool,
+	dockerhubAuth DockerHubAuth,
 ) []string {
-	logger.Info(fmt.Sprintf("Looking for version for %s:%s", sourceName, matchingString))
+	logger.Info(fmt.Sprintf("Looking for versions for %s:%s", sourceName, matchingString))
 
-	repository := strings.Join(strings.SplitN(sourceName, "/", 3)[1:], "/")
+	isQuay := strings.HasPrefix(sourceName, "quay.io/")
+
+	repoParts := strings.SplitN(sourceName, "/", 2)
+	if len(repoParts) != 2 {
+		fmt.Printf("Invalid source name format. Expected format: 'namespace/repo'. Got: %s\n", sourceName)
+		os.Exit(1)
+	}
+	repository := repoParts[1]
+
 	matchedTags := []string{}
 	page := 1
 
 	for {
-		// Update the URL to fetch the current page
-		url := fmt.Sprintf("https://quay.io/api/v1/repository/%s/tag/?limit=100&page=%d", repository, page)
-		resp, err := http.Get(url)
+		var url string
+		if isQuay {
+			url = fmt.Sprintf("https://quay.io/api/v1/repository/%s/tag/?limit=100&page=%d", repository, page)
+		} else {
+			url = fmt.Sprintf("https://hub.docker.com/v2/repositories/%s/tags/?page_size=100&page=%d", sourceName, page)
+		}
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fmt.Printf("Error creating request: %v\n", err)
+			os.Exit(1)
+		}
+
+		if !isQuay && dockerhubAuth.username != "" && dockerhubAuth.password != "" {
+			req.SetBasicAuth(dockerhubAuth.username, dockerhubAuth.password)
+		}
+
+		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
 			fmt.Printf("Error fetching data: %v\n", err)
 			os.Exit(1)
@@ -35,14 +64,16 @@ func ListVersion(
 
 		if resp.StatusCode != http.StatusOK {
 			fmt.Printf("Error: received status code %d\n", resp.StatusCode)
-			os.Exit(1)
+			break
 		}
 
-		// Decode the JSON response
 		var result struct {
 			Tags []struct {
 				Name string `json:"name"`
 			} `json:"tags"`
+			Results []struct {
+				Name string `json:"name"`
+			} `json:"results"`
 		}
 
 		if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
@@ -50,17 +81,29 @@ func ListVersion(
 			os.Exit(1)
 		}
 
-		if len(result.Tags) == 0 {
-			break
+		var tags []string
+		if isQuay {
+			if page > 10 || len(result.Tags) == 0 {
+				break
+			}
+			for _, tag := range result.Tags {
+				tags = append(tags, tag.Name)
+			}
+		} else {
+			if page > 10 || len(result.Results) == 0 {
+				break
+			}
+			for _, result := range result.Results {
+				tags = append(tags, result.Name)
+			}
 		}
 
-		// Generate the regex
 		regex := GenerateRegex(matchingString, allowCandidateRelease)
 		re := regexp.MustCompile(regex)
 
-		for _, tag := range result.Tags {
-			if re.MatchString(tag.Name) && !contains(matchedTags, tag.Name) {
-				matchedTags = append(matchedTags, tag.Name)
+		for _, tag := range tags {
+			if re.MatchString(tag) && !contains(matchedTags, tag) {
+				matchedTags = append(matchedTags, tag)
 			}
 		}
 
