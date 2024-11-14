1cec19fa91cc2f6c8f2d5c8315c17f08b2709382
Tchoupinax <corentinfiloche@hotmail.fr>
2024-11-14 14:27:09 +0100

    feat: display last execution of image in dashboard (#56)

--- ui/components/table-images.vue
+++ ui/components/table-images.vue
@@ -8,6 +8,7 @@
           <th class="px-6 py-3 text-left">Source/Destination</th>
           <th class="px-6 py-3 text-left">Status</th>
           <th class="px-6 py-3 text-left">Created At</th>
+          <th class="px-6 py-3 text-left">Last Execution</th>
         </tr>
       </thead>
       <tbody class="text-sm font-light text-gray-700">
@@ -15,7 +16,9 @@
           class="transition-colors border-b border-gray-200 hover:bg-gray-100">
           <td class="px-6 py-3 text-lg">{{ image.name }}</td>
           <td class="flex-col items-end justify-end px-6 py-3 text-lg">
-            <p>{{ image.source.name }}:{{ image.source.version }}</p>
+            <p>
+              {{ image.source.name }}:{{ image.source.version }}
+            </p>
             <!--<p>⬇</p>
             <p>{{ image.destination.name }}:{{ image.destination.version }}</p>-->
           </td>
@@ -23,17 +26,21 @@
             image.status }}
           </td>
           <td class="px-6 py-3 text-lg">{{ formatDate(image.createdAt) }}</td>
+          <td class="px-6 py-3 text-lg">{{ format(image.lastExecution) }}</td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { format } from 'timeago.js';
+
 export default {
   props: ["images"],
   methods: {
-    formatDate(dateString) {
+    format,
+    formatDate(dateString: string) {
       const options = { year: "numeric", month: "long", day: "numeric" };
       return new Date(dateString).toLocaleDateString(undefined, options);
     },
