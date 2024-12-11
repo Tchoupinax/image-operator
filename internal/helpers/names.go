63ca13b0501a61fb375d0dc90850e945758d10e7
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-11 15:48:45 +0100

    fix: handle when name finishes with a dash (#103)

--- internal/helpers/names.go
+++ internal/helpers/names.go
@@ -9,11 +9,26 @@ func GenerateSkopeoJobName(
 	name string,
 	version string,
 ) string {
-	return truncateString(fmt.Sprintf(
+	finalName := truncateString(fmt.Sprintf(
 		"image-operator-copy-%s-%s",
 		strings.ReplaceAll(strings.ReplaceAll(name, ".", "-"), "/", "-"),
 		strings.ReplaceAll(version, ".", "-"),
 	), 63)
+
+	return strings.TrimSuffix(finalName, "-")
+}
+
+func GenerateImageName(
+	name string,
+	version string,
+) string {
+	finalName := truncateString(fmt.Sprintf(
+		"%s-%s",
+		strings.ReplaceAll(strings.ReplaceAll(name, ".", "-"), "/", "-"),
+		strings.ReplaceAll(version, ".", "-"),
+	), 63)
+
+	return strings.TrimSuffix(finalName, "-")
 }
 
 func truncateString(s string, maxLen int) string {
