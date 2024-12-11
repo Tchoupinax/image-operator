5e84f79ca2cf9bebfc5c4e688386e2c3fb40d7b5
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-11 14:44:30 +0100

    fix: job name can't be longer than 63 characters (#102)

--- internal/helpers/names.go
+++ internal/helpers/names.go
@@ -9,9 +9,16 @@ func GenerateSkopeoJobName(
 	name string,
 	version string,
 ) string {
-	return fmt.Sprintf(
-		"skopeo-job-copy-%s-%s",
+	return truncateString(fmt.Sprintf(
+		"image-operator-copy-%s-%s",
 		strings.ReplaceAll(strings.ReplaceAll(name, ".", "-"), "/", "-"),
 		strings.ReplaceAll(version, ".", "-"),
-	)
+	), 63)
+}
+
+func truncateString(s string, maxLen int) string {
+	if len(s) > maxLen {
+		return s[:maxLen]
+	}
+	return s
 }
