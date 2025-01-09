12969f331a6b043b3ff408214fa178f5fe102956
Tchoupinax <corentinfiloche@hotmail.fr>
2025-01-09 10:58:43 +0100

    Improve test for Docker debugging session (#133)
    * Debug image
    
    * Improve regex
    
    * Handle dot in tag version
    
    * Improve test for Docker debugging session

--- internal/helpers/dockerhub_test.go
+++ internal/helpers/dockerhub_test.go
@@ -1,6 +1,8 @@
 package helpers_test
 
 import (
+	"fmt"
+	"net"
 	"regexp"
 	"strings"
 
@@ -10,9 +12,8 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func validIP4(ipAddress string) bool {
+func IsValidIP4(ipAddress string) bool {
 	ipAddress = strings.Trim(ipAddress, " ")
-
 	re, _ := regexp.Compile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
 	if re.MatchString(ipAddress) {
 		return true
@@ -20,13 +21,23 @@ func validIP4(ipAddress string) bool {
 	return false
 }
 
+func IsIPv6(ipAddress string) bool {
+	ip := net.ParseIP(ipAddress)
+	return ip != nil && strings.Contains(ipAddress, ":")
+}
+
+func isValidIp(ipAddress string) bool {
+	return IsValidIP4(ipAddress) || IsIPv6(ipAddress)
+}
+
 var _ = Describe("DockerHub", func() {
 	Describe("when getting remaining quota", func() {
 		It("should return data", func() {
 			var data = helpers.GetDockerhubLimit(logr.Logger{})
 
+			fmt.Printf("\n~ Remaining is %d.\n", data.Remaining)
 			Expect(data.Succeeded).To(BeTrue())
-			Expect(validIP4(data.Ip)).To(BeTrue())
+			Expect(isValidIp(data.Ip)).To(BeTrue())
 			Expect(data.Limit).To(Equal(100))
 			Expect(data.LimitWait).To(Equal(21600))
 			Expect(data.RemainingWait).To(Equal(21600))
